b0e51a9c93a4ce404f2b31ff8dc45fbc823ff6e2
napthats <thats525@gmail.com>
2014-07-20 12:05:55 +0900

    Add isConnected property to Client.

--- fsphidm/Program.fs
+++ fsphidm/Program.fs
@@ -7,14 +7,19 @@ open System.Collections.Generic
 [<EntryPoint>]
 let main _ =
     let client_List = new List<Client>();
-    //tentative
-    let rnd = System.Random()
     while true do
         Async.Sleep(1000) |> Async.RunSynchronously
         List.map (fun client -> client_List.Add(client)) (getNewClientList()) |> ignore
+        let remove_client_List = new List<Client>();
         for client in client_List do
-            match client.Read() with
-            //| None -> ()
-            | None -> if (rnd.Next(2) = 0) then (client.Disconnect()) //tentative
-            | Some msg -> for receiver_client in client_List do receiver_client.Write(msg)
+            if not client.isConnected
+            then
+                printf "disconnected" 
+                remove_client_List.Add(client)
+            else
+                match client.Read() with
+                | None -> ()
+                //| None -> if (rnd.Next(2) = 0) then (client.Disconnect()) //tentative
+                | Some msg -> for receiver_client in client_List do receiver_client.Write(msg)
+        client_List.RemoveAll(fun client -> remove_client_List.Contains(client)) |> ignore
     0
--- fsphidm/Server.fs
+++ fsphidm/Server.fs
@@ -11,39 +11,54 @@ type Client =
     abstract Read : unit -> option<string>
     abstract Write : string -> unit
     abstract Disconnect : unit -> unit
+    abstract isConnected : bool
 
-type private ReadWriteMailbox = MailboxProcessor<string> * MailboxProcessor<string option>
+type private WriteProtocol =
+    | Write of string
+    | Close
+
+type private ReadProtocol =
+    | Read of string
+    | Closed
+
+type private ReadWriteMailbox = MailboxProcessor<ReadProtocol> * MailboxProcessor<WriteProtocol>
 
 type private InternalClient(mbox : ReadWriteMailbox) =
+    let mutable is_connected = true
     interface Client with
-        member this.Read() = (fst mbox).TryReceive(0) |> Async.RunSynchronously
-        member this.Write(msg) = (snd mbox).Post(Some(msg))
-        member this.Disconnect() = (snd mbox).Post(None)
+        member this.Read() =
+            match (fst mbox).TryReceive(0) |> Async.RunSynchronously with
+                | Some(Read(msg)) -> Some(msg)
+                | Some(Closed) -> is_connected <- false; None
+                | None -> None
+        member this.Write(msg) = (snd mbox).Post(Write(msg))
+        member this.Disconnect() = (snd mbox).Post(Close)
+        member this.isConnected = is_connected
     
-
-let private write_work (client:TcpClient, inbox:MailboxProcessor<string option>) =
+let private write_work (client:TcpClient, inbox:MailboxProcessor<WriteProtocol>) =
     use stream = client.GetStream()
     use out = new StreamWriter(stream, AutoFlush = true)
     let cont = ref true
     try
         while !cont do
-            match inbox.Receive(0) |> Async.RunSynchronously with
-            | None -> cont := false
-            | Some(msg) -> out.WriteLine(msg)
+            match inbox.Receive() |> Async.RunSynchronously with
+            | Close -> cont := false
+            | Write(msg) -> out.WriteLine(msg)
     with
         _ -> ()
     client.Close() //Is it OK to Close twice?
 
-let private read_work (client:TcpClient, inbox:MailboxProcessor<string>, mailbox_write:MailboxProcessor<string option>) =
+let private read_work (client:TcpClient, inbox:MailboxProcessor<ReadProtocol>, mailbox_write:MailboxProcessor<WriteProtocol>) =
     use stream = client.GetStream()
     use inp = new StreamReader(stream)
     try
         while not inp.EndOfStream do
             match inp.ReadLine() with
-            | line -> inbox.Post(line)
+            | line -> inbox.Post(Read(line))
     with
         _ -> ()
-    mailbox_write.Post(None)
+    mailbox_write.Post(Close)
+    inbox.Post(Closed)
     client.Close()
 
  
@@ -52,10 +67,10 @@ let private server = new MailboxProcessor<ReadWriteMailbox>(fun server_inbox -> 
     do socket.Start()
     while true do
         let client = socket.AcceptTcpClient()
-        let mailbox_write = new MailboxProcessor<string option>(fun inbox ->
+        let mailbox_write = new MailboxProcessor<WriteProtocol>(fun inbox ->
             async {write_work(client, inbox)}
         )
-        let mailbox_read = new MailboxProcessor<string>(fun inbox ->
+        let mailbox_read = new MailboxProcessor<ReadProtocol>(fun inbox ->
             async {read_work(client, inbox, mailbox_write)}
         ) 
         mailbox_write.Start()
