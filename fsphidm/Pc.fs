dc6e9968856b44b0722f02f812c054d3b8321be6
napthats <thats525@gmail.com>
2014-08-14 16:35:51 +0900

    Move check_new_pc and act_all_pc to Pc.fs.

--- fsphidm/Pc.fs
+++ fsphidm/Pc.fs
@@ -2,8 +2,9 @@
 
 open Chara
 open PhiMap
+open Server
 open PhiClient
-//type PhiClient = PhiClient.PhiClient
+open System.Collections.Generic
 
 //Constructor should be private?
 type Pc(phi_client : PhiClient, first_pos : Position, first_dir: AbsoluteDirection, _name: string) =
@@ -27,3 +28,31 @@ type Pc(phi_client : PhiClient, first_pos : Position, first_dir: AbsoluteDirecti
     member this.Send msg = phi_client.Write msg
     //tentative
     member this.isConnected = phi_client.isConnected
+
+
+let private pc_List = new List<Pc>()
+let private pc_num_ref = ref 0
+
+let check_new_pc() =
+    List.map
+        (fun client ->
+            let pc = new Pc((createPhiClient(client)),(PhiMap.get_default_position ()),PhiMap.N,(sprintf "pc%d" !pc_num_ref)) in
+            pc.Send(SPRawMessage(sprintf "#name pc%d" !pc_num_ref))
+            pc.Send(SPRawMessage("#ex-notice land=a"))
+            pc.Send(SPRawMessage("#ex-notice area=a"))
+            (pc :> Chara).Look()
+            incr pc_num_ref
+            pc_List.Add(pc))
+        (getNewClientList())
+        |> ignore
+
+let act_all_pc() =
+    let remove_pc_List = new List<Pc>();
+    for pc in pc_List do
+        if not pc.isConnected
+        then
+            printf "disconnected" 
+            remove_pc_List.Add(pc)
+        else
+            (pc :> Chara).DoAction()
+    pc_List.RemoveAll(fun phiclient -> remove_pc_List.Contains(phiclient)) |> ignore
--- fsphidm/Program.fs
+++ fsphidm/Program.fs
@@ -1,43 +1,11 @@
 ﻿module Main
 
-open Server
-open PhiClient
 open Pc
-open Chara
-open System.Collections.Generic
-
 
 [<EntryPoint>]
 let main _ =
-    //tentative
-    //printf "%s" <| PhiMap.get_sight_string(PhiMap.get_default_position (), PhiMap.E, 5)
-    //
-    //tentative: it should be chara_List
-    //tentative: deal with disappearing at disconnect
-    let pc_List = new List<Pc>()
-    //tentative: for name id
-    let pc_num_ref = ref 0
-    //let phiclient_List = new List<PhiClient>()
     while true do
         Async.Sleep(100) |> Async.RunSynchronously
-        List.map
-            (fun client ->
-                let pc = new Pc((createPhiClient(client)),(PhiMap.get_default_position ()),PhiMap.N,(sprintf "pc%d" !pc_num_ref)) in
-                pc.Send(SPRawMessage(sprintf "#name pc%d" !pc_num_ref))
-                pc.Send(SPRawMessage("#ex-notice land=a"))
-                pc.Send(SPRawMessage("#ex-notice area=a"))
-                (pc :> Chara).Look()
-                incr pc_num_ref
-                pc_List.Add(pc))
-            (getNewClientList())
-            |> ignore
-        let remove_pc_List = new List<Pc>();
-        for pc in pc_List do
-            if not pc.isConnected
-            then
-                printf "disconnected" 
-                remove_pc_List.Add(pc)
-            else
-                (pc :> Chara).DoAction()
-        pc_List.RemoveAll(fun phiclient -> remove_pc_List.Contains(phiclient)) |> ignore
+        check_new_pc()
+        act_all_pc()
     0
